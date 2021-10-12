a78d32f94b8b439b8cc1e225d05a301f957f5431
nakayama91 <k.naka10ry@gmail.com>
2021-10-12 05:24:22 +0000

    登録フォームの作成

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,12 @@
 class ApplicationController < ActionController::Base
+    
+    #deviseコントローラーの修正
+    #初期設定では用意されていないカラムを追加したので、下記を追記
+    before_action :configure_permitted_parameters, if: :devise_controller?
+    
+    protected
+    
+    def configure_permitted_parameters
+        devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+    end
 end
