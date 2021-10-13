fd7efabe6ffafc41cb66ce4afbf20f3f6bb17465
nakayama91 <k.naka10ry@gmail.com>
2021-10-13 01:38:56 +0000

    ヘッダー

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,6 @@ Rails.application.routes.draw do
   get 'users/show'
   devise_for :users
   root to: 'homes#top'
+  get 'home/about' => 'homes#about'
   resources :books, only: [:create, :index, :show, :edit, :destroy]
 end
