d336428707640910e28047edb50453513bdac221
nakayama91 <k.naka10ry@gmail.com>
2021-10-13 07:28:07 +0000

    header完了

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,6 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'homes#top'
   get 'home/about' => 'homes#about'
+  get 'users/index' => 'users#index'
   resources :books, only: [:create, :index, :show, :edit, :destroy]
 end
