1c3fbe36236186a6e11d62931e942fcf2eda5b98
nakayama91 <k.naka10ry@gmail.com>
2021-10-24 07:35:46 +0000

    フォローアクション記述

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,13 @@ Rails.application.routes.draw do
 
   root to: 'homes#top'
   get 'home/about' => 'homes#about'
-  resources :users
+  
+  resources :users do
+    resource :relationships, only: [:create, :destroy]
+    get 'followings' => 'relationships#followings', as:'followings'
+    get 'followers' => 'relationships#followers', as:'followers'
+  end
+    
   resources :books do
     resources :book_comments, only: [:create, :destroy]
     resource :favorites, only: [:create, :destroy]
