71457f6989258e2460f48417966d0b3365db5bda
nakayama91 <k.naka10ry@gmail.com>
2021-10-25 02:53:21 +0000

    ~

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,17 +7,17 @@ class User < ApplicationRecord
   has_many :books, dependent: :destroy
   has_many :favorites, dependent: :destroy
   has_many :book_comments, dependent: :destroy
-  
-  has_many :relationships, class_name:"Relationship", foreign_key:"followed_id", dependent: :destroy
+
+  has_many :followings, class_name:"Relationship", foreign_key:"followed_id", dependent: :destroy
   has_many :followers, through: :relationships, source: :follower
-  has_many :relationships, class_name:"Relationship", foreign_key:"follower_id", dependent: :destroy
+  has_many :followers, class_name:"Relationship", foreign_key:"follower_id", dependent: :destroy
   has_many :followings, through: :relationships, source: :followed
 
   attachment :profile_image
 
   validates :name, length: { minimum: 2, maximum: 20 }, uniqueness: true
   validates :introduction, length: { maximum: 50 }
-  
+
   def follow(user_id)
     relationships.create(followed_id: user_id)
   end
@@ -25,7 +25,7 @@ class User < ApplicationRecord
     relationships.find_by(followed_id: user_id).destroy
   end
   def following?(user)
-    followings.include?(user)
+    followed.include?(user)
   end
 
 end
